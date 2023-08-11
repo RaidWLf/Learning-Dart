2e6d3ad0f412e99cf1caa561101a7464bb197fb2
Ambuj Kumar <ambuj.kumar.7903@gmail.com>
2023-08-11 12:51:40 +0000

    Learned IfElse Ternary & Switch Case Statements

---
+++ 03Control_Flow/02Ternary.dart
@@ -0,0 +1,12 @@
+void main() {
+  /*
+  Ternary Operators is short hand for an if else statement
+
+  Syntax =>  Condition to be checked ? ifTrue : ifFalse ;
+  */
+  String greeting = 'Hi!';
+
+  String isCorrect = greeting.startsWith("H") ? 'yes' : "no";
+
+  print(isCorrect);
+}
---
+++ 03Control_Flow/03SwitchStatment.dart
@@ -0,0 +1,13 @@
+void main() {
+  int age = 30;
+
+  String name = "RaidWLf";
+
+  switch (name) {
+    case "RaidWLf" when (age == 30):
+      print("His name is $name and age is $age");
+      break;
+    default:
+      print('name and age is not correct ');
+  }
+}
